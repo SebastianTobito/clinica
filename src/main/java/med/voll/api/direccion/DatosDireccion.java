cc15cda20be35f34935aa87f5951761637d7c6ef
Sebastian Tobito <sebastiantobito@gmail.com>
2025-03-18 16:30:25 -0500

    subiendo las clases MedicoController, DatosDireccion, DatosRegistroMedico y especialidad

---
+++ src/main/java/med/voll/api/direccion/DatosDireccion.java
@@ -0,0 +1,8 @@
+package med.voll.api.direccion;
+
+public record DatosDireccion(String calle,
+                             String distrito,
+                             String ciuda,
+                             String numero,
+                             String complemento) {
+}
