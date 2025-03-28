978a536b7c00408e7b96efb5da2835798a628e9d
Sebastian Tobito <sebastiantobito@gmail.com>
2025-03-28 14:30:19 -0500

    agrandodo clases medico y direccion y el MedicoRepository

--- src/main/java/med/voll/api/direccion/DatosDireccion.java
+++ src/main/java/med/voll/api/direccion/DatosDireccion.java
@@ -1,8 +1,15 @@
 package med.voll.api.direccion;
 
-public record DatosDireccion(String calle,
+import jakarta.validation.constraints.NotBlank;
+
+public record DatosDireccion(@NotBlank
+                             String calle,
+                             @NotBlank
                              String distrito,
-                             String ciuda,
+                             @NotBlank
+                             String ciudad,
+                             @NotBlank
                              String numero,
+                             @NotBlank
                              String complemento) {
 }
